3364123e39b0a0dfa82b822ceb4baa0d947038ef
dimakulik <vezyn4ikplay@gmail.com>
2026-01-12 21:22:44 +0300

    Update app.js

--- app.js
+++ app.js
@@ -1,8 +1,28 @@
+const cells = [
+    { x: 20, y: 20 },
+    { x: 130, y: 20 },
+    { x: 240, y: 20 },
+    { x: 350, y: 20 },
+    { x: 350, y: 130 },
+    { x: 350, y: 240 },
+    { x: 240, y: 350 },
+    { x: 130, y: 350 }
+];
+
 let position = 0;
+const player = document.getElementById("player");
 const log = document.getElementById("log");
+
+function movePlayer() {
+    player.style.left = cells[position].x + "px";
+    player.style.top = cells[position].y + "px";
+}
 
 document.getElementById("roll").onclick = () => {
     const dice = Math.floor(Math.random() * 6) + 1;
-    position = (position + dice) % 4;
-    log.innerText = `🎲 Выпало ${dice}. Ты на клетке ${position}`;
+    position = (position + dice) % cells.length;
+    movePlayer();
+    log.innerText = `🎲 Выпало ${dice}`;
 };
+
+movePlayer();
